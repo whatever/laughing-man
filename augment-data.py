9912dad331a0a6c0794430238d25b3bd7f67b929
matt 3 <matt@worldshadowgovernment.com>
2023-10-03 09:28:23 -0400

    :earth_africa: generate a bunch of flipped/cropped/etc images for training

--- augment-data.py
+++ augment-data.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plot
 import numpy as np
 import json
 import numpy
+import os.path
 import torch
 import torchvision
 
@@ -116,10 +117,89 @@ def show_augmented_image(aug):
     show_image(img)
 
 
+def get_standardized_bbox(label_fname):
+    """Convert a bounding box to the standard format"""
+
+    with open(label_fname, "r") as f:
+        label = json.load(f)
+
+    shapes = label["shapes"]
+
+    assert len(shapes) == 1
+
+    shape = shapes[0]
+
+    return [
+        bbox[0],
+        bbox[1],
+        bbox[0] + bbox[2],
+        bbox[1] + bbox[3],
+    ]
+
+
+def get_label_fname(image_fname):
+    return (
+        image_fname
+        .replace("images", "labels")
+        .replace(".jpg", ".json")
+    )
+
+
+def get_image_and_label(image_fname):
+
+    label_fname = get_label_fname(image_fname)
+
+    with open(label_fname, "r") as fi:
+        label = json.load(fi)
+
+    shape = label.get("shapes", [None])[0]
+
+
+    img = Image.open(image_fname).convert("RGB")
+    w, h = img.size
+
+
+    if not shape:
+        return {
+            "image_fname": image_fname,
+            "label_fname": label_fname,
+            "bbox": [0, 0, 0.000001,  0.000001],
+            "class": 0,
+        }, img
+
+    p0, p1 = shape["points"]
+
+    coords = [
+        min(p0[0], p1[0]),
+        min(p0[1], p1[1]),
+        max(p0[0], p1[0]),
+        max(p0[1], p1[1]),
+    ]
+
+    bb = [
+        coords[0] / w,
+        coords[1] / h,
+        coords[2] / w,
+        coords[3] / h,
+    ]
+
+    class_label = int(shape["label"] == "matt-face")
+
+    res = {
+        "image_fname": image_fname,
+        "label_fname": label_fname,
+        "bbox": bb,
+        "class": class_label,
+    }
+
+    return res, img
+
+
 
 if __name__ == "__main__":
 
 
+    """
     for fname in glob("data/test/labels/*.json"):
         with open(fname, "r") as fi:
             label = json.load(fi)
@@ -129,16 +209,6 @@ if __name__ == "__main__":
 
 
         shape = shapes[0]
-
-        p0 = shape["points"][0]
-        p1 = shape["points"][1]
-
-        coords = [
-            min(p0[0], p1[0]),
-            min(p0[1], p1[1]),
-            max(p0[0], p1[0]),
-            max(p0[1], p1[1]),
-        ]
 
         int_coords = [int(p) for p in coords]
 
@@ -174,6 +244,73 @@ if __name__ == "__main__":
 
 
     cv2.destroyAllWindows()
+    """
+
+
+    # Modify data
+
+
+    for partition in ["train", "test", "validate"]:
+        for image_fname in glob(f"data/{partition}/images/*.jpg"):
+            res, img = get_image_and_label(image_fname)
+
+            for i in range(60):
+                aug = augmentor(
+                    image=np.array(img),
+                    bboxes=[res["bbox"]],
+                    class_labels=["face"],
+                )
+
+                aug_basename = "{}-{}.json".format(
+                    os.path.basename(image_fname).replace(".json", ""),
+                    i,
+                )
+
+                aug_image_fname = os.path.join(
+                    "aug_data",
+                    partition,
+                    "images",
+                    f"{aug_basename}.jpg",
+                )
+
+                aug_label_fname = os.path.join(
+                    "aug_data",
+                    partition,
+                    "labels",
+                    f"{aug_basename}.json",
+                )
+
+                annotation = {
+                    "bbox": res["bbox"],
+                    "class": res["class"],
+                }
+
+                cv2.imwrite(aug_image_fname, aug["image"])
+
+                with open(aug_label_fname, "w") as f:
+                    json.dump(annotation, f)
+
+
+    raise SystemExit
+
+    for partition in ["train", "test", "validate"]:
+        for label_fname in glob(f"data/{partition}/labels/*.json"):
+            image_fname = label_fname.replace("labels", "images").replace(".json", ".jpg")
+            aug_fname = os.path.join("aug_data", partition, "labels", os.path.basename(label_fname))
+
+
+
+            if not os.path.exists(image_fname):
+                annotation["bbox"] = [0]*4
+                annotation["class"] = 0
+
+            with open(aug_fname, "w") as f:
+                # json.dump(aug_
+                pass
+
+            print(label_fname)
+            print(image_fname)
+            print(aug_fname)
 
 
 
