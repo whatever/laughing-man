f5e6d597e294a07bd9416ca5951fec6d84a4d1a3
matt 3 <matt@worldshadowgovernment.com>
2023-10-05 16:03:33 -0400

    :wolf: split and augment at the same time

---
+++ laughing_person/__init__.py
@@ -0,0 +1,28 @@
+import albumentations as alb
+
+
+def get_label_fname(image_fname):
+    return (
+        image_fname
+        .replace("images", "labels")
+        .replace(".jpg", ".json")
+    )
+
+CROP_WIDTH = CROP_HEIGHT = 1000
+
+ts = [
+    alb.SmallestMaxSize(max_size=min(CROP_WIDTH, CROP_HEIGHT)),
+    alb.RandomCrop(width=CROP_WIDTH, height=CROP_HEIGHT),
+    alb.HorizontalFlip(p=0.5),
+    alb.VerticalFlip(p=0.5),
+    alb.RandomBrightnessContrast(p=0.2),
+    alb.RandomGamma(p=0.2),
+    alb.RGBShift(p=0.2),
+]
+
+bbox_params = alb.BboxParams(
+    format="albumentations",
+    label_fields=["class_labels"],
+)
+
+augmentor = alb.Compose(ts, bbox_params)
