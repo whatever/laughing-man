d3c16648feab493929b1a63fe8e04749f976b108
matt 3 <matt@worldshadowgovernment.com>
2023-10-06 11:08:57 -0400

    :camera: show results as you go

--- benchmark.py
+++ benchmark.py
@@ -6,15 +6,17 @@ import cv2
 import laughing_person as lp
 import logging
 import json
+import matplotlib.pyplot as plot
 import numpy as np
-import torch
+import random
 import signal
+import torch
 
 
 from datetime import timedelta
 from datetime import datetime
 from glob import glob
-from train import dataset, IsMattModule, get_label_fname, load_label
+from train import dataset, IsMattModule, get_label_fname, load_label, show_image
 from PIL import Image
 
 face_cascade = cv2.CascadeClassifier('capture-images/haarcascade_frontalface_default.xml')
@@ -75,34 +77,80 @@ if __name__ == "__main__":
     model = IsMattModule()
 
     for model_fname in sorted(glob(f"{args.checkpoints_dir}/*.pt")):
-        logger.info("Loading model %s", model_fname)
+        # logger.info("Loading model %s", model_fname)
         checkpoint = torch.load(model_fname)
         model.load_state_dict(checkpoint['model_state_dict'])
         model.eval()
         model = model.cuda()
 
-        logging.info("Validating model %s", model_fname)
+        # logging.info("Validating model %s", model_fname)
 
-        examples = sorted(fname for fname in glob("data/train/images/*.jpg"))
-        examples = examples[0:1]
+        examples = sorted(fname for fname in glob(f"{args.validate_dir}/*.jpg"))
+        random.shuffle(examples)
+        examples = examples[0:10]
 
+
+        images = []
 
         for image_fname in examples:
             label_fname = get_label_fname(image_fname)
+
             with open(label_fname, "r") as fi:
                 label = json.load(fi)
-
-            is_face_actual = len(label["shapes"])
+                print(json.dumps(label, indent=4))
 
             img = Image.open(image_fname)
-            face, bb = model.predict(img)
-            p, q = label["shapes"][0]["points"]
-            predicted_bb = p + q
+            o = lp.crop(img)
+            o = np.array(o)
 
+            face, predicted_bb = model.predict(img)
+            actual_bb = label["bbox"]
+            actual_bb = actual_bb if len(actual_bb) == 4 else actual_bb[0]
+
+            is_face_actual = label["class"]
 
             w, h = img.size
+            w, h, _ = o.shape
+
+            y = [
+                int(v)
+                for v in (np.array(actual_bb)*np.array([w, h, w, h])).tolist()
+            ]
+
+            y_hat = [
+                int(v)
+                for v in (predicted_bb[0].cpu().numpy()*np.array([w, h, w, h])).tolist()
+            ]
 
 
-            y = predicted_bb
-            y_hat = (bb[0].cpu().numpy()*np.array([w, h, w, h])).tolist()
-            print(image_fname, calc_iou(y, y_hat))
+            o = lp.crop(img)
+            o = np.array(o)
+            cv2.rectangle(o, (y[0], y[1]), (y[2], y[3]), (0, 255, 0), 2)
+            cv2.rectangle(o, (y_hat[0], y_hat[1]), (y_hat[2], y_hat[3]), (0, 255, 255), 2)
+
+            images.append(o.copy())
+
+
+        arr = images[0]
+
+        for i in range(1, len(images)):
+            arr = np.concatenate((arr, images[i]), axis=1)
+
+        arr = cv2.cvtColor(arr, cv2.COLOR_BGR2RGB)
+        cv2.imshow("img", arr)
+
+        print(o.shape)
+
+
+        print(f"image .... {image_fname}")
+        print(f"y ........ {y}")
+        print(f"y_hat .... {y_hat}")
+        print()
+        cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
+
+
+            # plot.figure()
+            # plot.imshow(img)
+            # n plot.show()
